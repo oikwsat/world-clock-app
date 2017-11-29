9faf78e79cb74dd59fe8b66ec0e8cec572b09145
Satoshi Oikawa <oikwsat@gmail.com>
2017-11-30 00:45:16 +0900

    display current date and time

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import moment from "moment";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: moment().format("LTS"),
+      date: moment().format("ll"),
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        time: moment().format("LTS"),
+        date: moment().format("ll"),
+      })
+    }, 1000);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>World-Clock-App!</Text>
+        <Text style={styles.timeText}>{this.state.time}</Text>
+        <Text style={styles.dateText}>{this.state.date}</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -19,5 +40,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  timeText: {
+    color: '#666',
+    fontSize: 60,
+  },
+  dateText: {
+    color: '#666',
+    fontSize: 30,
   },
 });
