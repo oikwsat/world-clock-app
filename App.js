f496188481adfef94b9ce12645c0bb9a1a9182b9
Satoshi Oikawa <oikwsat@gmail.com>
2017-11-30 23:15:12 +0900

    add concept of timezone and componentize clock

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import moment from "moment";
+import moment from "moment-timezone";
 
-export default class App extends React.Component {
+class Clock extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      time: moment().format("LTS"),
-      date: moment().format("ll"),
+      time: moment().tz(props.timezone).format("HH:mm:ss"),
+      date: moment().tz(props.timezone).format("ll"),
     };
-  }
 
-  componentDidMount() {
     setInterval(() => {
       this.setState({
-        time: moment().format("LTS"),
-        date: moment().format("ll"),
+        time: moment().tz(props.timezone).format("HH:mm:ss"),
+        date: moment().tz(props.timezone).format("ll"),
       })
     }, 1000);
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>World-Clock-App!</Text>
+      <View style={styles.clock}>
         <Text style={styles.timeText}>{this.state.time}</Text>
         <Text style={styles.dateText}>{this.state.date}</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text style={styles.dateText}>{this.props.timezone}</Text>
+      </View>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>World-Clock-App!</Text>
+        <Clock style={{flex: 1}} timezone='Asia/Tokyo' />
+        <Clock style={{flex: 2}} timezone='Europe/London' />
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
@@ -38,6 +47,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  clock: {
+    flex: 1,
+    backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center',
   },
