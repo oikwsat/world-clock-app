c47d1ffafe8163588e8deb8447b8f1d9e01c356c
Satoshi Oikawa <oikwsat@gmail.com>
2017-12-06 23:57:56 +0900

    arrange the UI components in Settings

--- SettingScreen.js
+++ SettingScreen.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Switch, View } from 'react-native';
 
 export default class SettingScreen extends React.Component {
   static navigationOptions = {
     title: 'Settings',
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // 12 or 24 hour display mode
+      hourDisplayMode: true,
+      // display seconds display or undisplay
+      showSeconds: false,
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>24時間形式を使用</Text>
-        <Text>秒を表示する</Text>
+        <View>
+          <Text style={styles.text}>24時間形式を使用</Text>
+          <Switch
+            style={styles.switch}
+            onValueChange = {(value) => this.setState({hourDisplayMode: value})}
+            value = {this.state.hourDisplayMode} />
+          <Text style={styles.text}>秒を表示する</Text>
+          <Switch
+            style={styles.switch}
+            onValueChange = {(value) => this.setState({showSeconds: value})}
+            value = {this.state.showSeconds} />
+        </View>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
@@ -21,7 +42,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    margin: 20
+  },
+  switch: {
+    margin: 20,
+    marginTop: -41
   },
 });
